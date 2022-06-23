638d133dda67f8a0f0793261088074892007a888
Vladimir814 <vladimir_814@ukr.net>
2022-06-23 18:50:59 +0300

    Add all in function main

--- task2/main.go
+++ task2/main.go
@@ -9,42 +9,34 @@ import (
 
 func main() {
 	input := "1 9 3 4 -5"
-	 var result string
+	var result string
 
-	 arrString := strings.Split(input, " ")
-	 arr := make([]int, len(arrString))
-	 i := 0
-	 for _,v := range arrString {
-		x,err := strconv.Atoi(v)
-	    if err != nil {
-		    panic(err)
-	    }
-	    arr[i] = x
+	arrString := strings.Split(input, " ")
+	arr := make([]int, len(arrString))
+	i := 0
+	for _, v := range arrString {
+		x, err := strconv.Atoi(v)
+		if err != nil {
+			panic(err)
+		}
+		arr[i] = x
 		i++
 	}
 
-	var minValue = min(arr)
-	var maxValue = max(arr)
+	var minValue = math.MaxInt32
+	for _, v := range arr {
+		if v < minValue {
+			minValue = v
+		}
+	}
+
+	var maxValue = math.MinInt32
+	for _, v := range arr {
+		if v > maxValue {
+			maxValue = v
+		}
+	}
+
 	result = fmt.Sprint(maxValue, " та ", minValue)
 	fmt.Println(result)
-}
-
-func min(arr []int) int {
-	var res = math.MaxInt32
-	for _, v := range arr {
-		if v < res {
-			res = v
-		}
-	}
-	return res
-}
-
-func max(arr []int) int {
-	var res = math.MinInt32
-	for _, v := range arr {
-		if v > res {
-			res = v
-		}
-	}
-	return res
 }
